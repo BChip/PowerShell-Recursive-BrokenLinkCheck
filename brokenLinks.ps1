9caafbc006966a990382faad4affd4802f352b96
BChip <bachippi@svsu.edu>
2016-02-19 13:46:36 -0500

    Minor Fixes to Improve Results

--- brokenLinks.ps1
+++ brokenLinks.ps1
@@ -1,22 +1,24 @@
-﻿Clear-Host
-Stop-Process -name WINWORD
+﻿Stop-Process -name WINWORD
+Clear-Host
 $path = 'C:\'
 $files = Get-Childitem $path -Include *.docx,*.doc -Recurse | Where-Object { !($_.psiscontainer) }
 $application = New-Object -comobject word.application
 $application.visible = $False
-'Location:'| Add-Content -path 'deadLinks.csv'
+'Location:' + "," + "Text:" + "," + "Link:" + "," + "Try Failed?" + "," + "Error Report" + "," + "SharePoint Access:" | Add-Content -path 'deadLinks.csv'
 
 Function Get-StringMatch
 {
     # Loop through all *.doc files in the $path directory
     Foreach ($file In $files)
     {
+        Write-Host $file
         $document = $application.documents.open($file.fullname,$false,$true)
         $hyperlinks = @($document.Hyperlinks) 
         foreach($hyperlink In $hyperlinks) 
         {
             
-            try{ 
+            try{
+                #check for links that are blank
                 if(!$hyperlink.Address){
                     continue
                 }
@@ -26,26 +28,21 @@ Function Get-StringMatch
                 }
                 #check if website works
                 elseif(($hyperlink.Address -match "http") -eq $true){
-                    $HTTP_Request = [System.Net.WebRequest]::Create($hyperlink.Address)
-                    $HTTP_Response = $HTTP_Request.GetResponse()
-                    $HTTP_Status = [int]$HTTP_Response.StatusCode
-                    if ($HTTP_Status -eq 200) { 
-                        Write-Host "Site is OK!" 
+                    $r = Invoke-WebRequest $hyperlink.Address -UseDefaultCredentials
+                    if ($r.StatusCode -eq 200) {
+                        if($r.Content.IndexOf("Sorry,") -ne -1){
+                            $file.fullname + "," + $hyperlink.TextToDisplay + "," + $hyperlink.Address + "," + "NULL" + "," + "NULL" + "," + "ACCESS DENIED" | Add-Content -path 'deadLinks.csv' 
+                        } 
                     }
-                    else {
-                        $file.fullname + "," + $hyperlink.TextToDisplay + "," + $hyperlink.Address | Add-Content -path 'deadLinks.csv'
-                    }
-                    $HTTP_Response.Close()
                 }
                 else{
                     #try and test the path to a directory
-                
                     if ((Test-Path $hyperlink.Address -errorAction Stop) -eq $false) { 
                         $file.fullname + "," + $hyperlink.TextToDisplay + "," + $hyperlink.Address | Add-Content -path 'deadLinks.csv' 
                     }
                 }
             }catch{
-                $file.fullname + "," + $hyperlink.TextToDisplay + "," + $hyperlink.Address | Add-Content -path 'deadLinks.csv' 
+                $file.fullname + "," + $hyperlink.TextToDisplay + "," + $hyperlink.Address + "," + "True" + "," + $_.Exception.Message | Add-Content -path 'deadLinks.csv' 
                 continue
             }
             
